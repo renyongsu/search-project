cdef79705279340d3b34bddbc143669d51c36361
renyongsu <surenyong_andy@163.com>
2012-03-19 09:52:40 +0800

    modify

---
+++ search-core/src/main/java/com/sohu/wap/search/core/index/DefaultDocumentFactory.java
@@ -0,0 +1,27 @@
+package com.sohu.wap.search.core.index;
+
+import java.util.List;
+
+import org.apache.lucene.document.Document;
+
+public class DefaultDocumentFactory implements DocumentFactory{
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Document> getDocuments() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
