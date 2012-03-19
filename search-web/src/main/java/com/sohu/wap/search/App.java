6e7e3aa114a5cc4811f21edee33788f38e773351
renyongsu <surenyong_andy@163.com>
2012-03-19 13:43:20 +0800

    add web module

---
+++ search-web/src/main/java/com/sohu/wap/search/App.java
@@ -0,0 +1,13 @@
+package com.sohu.wap.search;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
